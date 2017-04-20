039004371cbc2a38208afef79a604b933b4d2f15
Abner Oliveira <abner.silva@gmail.com>
2017-04-20 18:10:00 -0300

    added loading indicator component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { Observer } from 'rxjs/Observer';
+import 'rxjs/add/observable/of';
+import 'rxjs/add/operator/delay';
+
+declare var require;
 
 @Component({
   selector: 'app-root',
   template: require('./app.component.html')
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+  observable: Observable<any>;
+  observer: Observer<any>;
+  ngOnInit() {
+    // this.observable = Observable.of(1).delay(4000);
+    this.observable = new Observable((observer) => {
+      this.observer = observer;
+    });
+  }
+
+  complete() {
+    this.observer.complete();
+  }
+
+  error() {
+    this.observer.error(new Error('Erro durante a execução'));
+  }
+  subscribe() {
+    this.observable.subscribe(() => {
+      alert('COMPLETOU');
+    }, (e) => {
+      alert(e.message);
+    });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 
+import { NgxSerproUiModule } from 'ngx-serpro';
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    NgxSerproUiModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
